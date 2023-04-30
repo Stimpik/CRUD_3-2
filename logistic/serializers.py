e6a19bfac24fa90c90cd58af44b7ab59f9025a76
Igor D <iderik@mail.ru>
2023-04-30 23:39:53 +0300

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+    # настройте сериализатор для позиции продукта на складе
+
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions', 'id']
+
+    def create(self, validated_data):
+
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock,
+                                                  product=position['product'],
+                                                  defaults={
+                                                      'quantity': position['quantity'],
+                                                      'price': position['price']
+                                                  }
+                                                  )
+
+        return stock
+
